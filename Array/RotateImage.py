57eb0d2329c4ac5947374969f9d58a0f2946fdd0
Jonathan Birnbaum <yonatanb95@gmail.com>
2021-06-18 23:07:13 +0300

    Update RotateImage.py

--- Array/RotateImage.py
+++ Array/RotateImage.py
@@ -9,3 +9,20 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
+        n = len(matrix[0])
+        for i in range((n % 2) + (n // 2)):
+            for j in range(n // 2):
+                save_temp = matrix[j][i]
+                matrix[j][i] = matrix[n - 1 - i][j]
+                matrix[n - 1 - i][j] = matrix[n - 1 - j][n - 1 - i]
+                matrix[n - 1 - j][n - 1 - i] = matrix[i][n - 1 - j]
+                matrix[i][n - 1 - j] = save_temp
+        return matrix
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    # print(solution.rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+    # print(solution.rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]))
+    # print(solution.rotate([[1]]))
+    print(solution.rotate([[1,2],[3,4]]))
